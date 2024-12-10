971acce2c7e7a22235792181ed83ebe3ce0a1ac3
rioccrz <riocasscrz@gmail.com>
2024-12-11 02:25:43 +0800

    Added Students Components

--- src/pages/Student.jsx
+++ src/pages/Student.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import StudentSearchField from './student-components/student-basic-components/StudentSearchField'
+import StudentProfile from './student-components/student-profile-components/StudentProfile'
+import StudentQuizPage from './student-components/student-quiz-page-components/StudentQuizPage'
+import StudentClassPage from './student-components/student-class-page-components/StudentClassPage'
+import StudentClassPageOverview from './student-components/student-class-page-components/student-class-page-view-components/student-class-page-view-sub-components/StudentClassPageOverview'
+import StudentTakingQuiz from './student-components/student-quiz-page-components/student-quiz-page-sub-components/StudentTakingQuiz'
+import StudentClassPageView from './student-components/student-class-page-components/student-class-page-view-components/StudentClassPageView'
+import StudentClassPageQuiz from './student-components/student-class-page-components/student-class-page-view-components/student-class-page-view-sub-components/StudentClassPageQuiz'
+import StudentShowCorrect from './student-components/student-quiz-page-components/student-quiz-page-sub-components/StudentQuizShowCorrect'
+import StudentQuizResult from './student-components/student-quiz-page-components/student-quiz-page-sub-components/StudentQuizResult'
 const Student = () => {
   return (
-    <div>Student Page</div>
+    // <div>Student Page</div>
+    <StudentQuizResult/>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,34 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
       ],
     theme: {
-        extend: {},
-    },
-    plugins: [],
+        extend: {
+            fontFamily: {
+              lexend: ['Lexend', 'sans-serif'],
+            },
+            colors: {
+              primary: {
+                1: 'var(--primary-1)',
+                2: 'var(--primary-2)',
+                3: 'var(--primary-3)',
+              },
+              secondary: {
+                1: 'var(--secondary-1)',
+                2: 'var(--secondary-2)',
+              },
+              accent: {
+                1: 'var(--accent-1)',
+                2: 'var(--accent-2)',
+              },
+              text: {
+                1: 'var(--text-1)',
+                2: 'var(--text-2)',
+              },
+              positive: 'var(--positive)',
+              negative: 'var(--negative)',
+            },
+      
+      },
+  },
+  plugins: [],
 }
 
